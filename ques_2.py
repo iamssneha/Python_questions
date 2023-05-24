206e8fc5544ee6582c7b67b5b9df9aadae12b670
iamssneha <113632419+iamssneha@users.noreply.github.com>
2023-05-24 20:54:35 +0530

    Update ques_2.py

--- ques_2.py
+++ ques_2.py
@@ -9,6 +9,7 @@ sub5 = int(input())
 total = sub1+sub2+sub3+sub4+sub5
 print("total == 500")
 
+# if-else-question
 if total>=400 and total <=500:
     print("Grade: A")
 elif total>=300 and total<400:
@@ -17,6 +18,5 @@ elif total>=200 and total<300:
     print("Grade: C")
 elif total>=100 and total<200:
     print("Grade: D")
-
 else:
     print("Grade: F")
